19217e02159372c05c9f58698ed2fa70c2ca7271
w3villa-prince <prince.yadav@w3villa.com>
2023-01-25 15:49:43 +0530

    last commit

--- Assets/Scripts/PlatForm Scripts/PlatFormScripts.cs
+++ Assets/Scripts/PlatForm Scripts/PlatFormScripts.cs
@@ -1,8 +1,9 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlatFormScripts : MonoBehaviour
 {
-    public float moveSpeed = 2f;
+    public float moveSpeed = .009f;
     public float boundY = 6;
     public bool movingPlatformLeft, movingPlatformRight, isBreakable, isSpike, isPlatform;
 
@@ -13,26 +14,36 @@ public class PlatFormScripts : MonoBehaviour
     {
         if (isBreakable)
         {
+            Debug.Log("Animation Calling....");
             anim = GetComponent<Animator>();
         }
     }
 
     private void Update()
     {
+        StartCoroutine(Co_Move());
+    }
+
+    private IEnumerator Co_Move()
+    {
         while (gamePlay)
         {
+            Debug.Log("Move Method Calling");
             Move();
+            yield return null;
         }
     }
 
     private void Move()
     {
+        Debug.Log("Move Method call");
         Vector2 temp = transform.position;
         temp.y += moveSpeed * Time.deltaTime;
         transform.position = temp;
         if (temp.y >= boundY)
         {
-            gameObject.SetActive(false);
+            Destroy(gameObject);
+            gamePlay = false;
         }
     }
 }
--- Assets/Scripts/PlatForm Scripts/SpawnManager.cs
+++ Assets/Scripts/PlatForm Scripts/SpawnManager.cs
@@ -3,30 +3,37 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] platformPrefabs;
-    private float starSpawner = 1.5f;
-    private float intervalSpawner = 1f;
+    private float starSpawner = 0.01f;
+    private float intervalSpawner = 4f;
     private float max = 2;
     public bool gameplay = true;
 
     // Start is called before the first frame update
     private void Start()
     {
-        InvokeRepeating("SpawnRandomPlatform", starSpawner, intervalSpawner);
-    }
-
-    private void SpawnRandomPlatform()
-    {
-        int platformIndex = Random.Range(0, platformPrefabs.Length - 1);
-        Vector3 pos = new Vector3(Random.Range(-max, max), 6, 10);
-        Instantiate(platformPrefabs[platformIndex], pos, platformPrefabs[platformIndex].transform.rotation);
+        InvokeRepeating(nameof(SpawnRandomPlatform), starSpawner, intervalSpawner);
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (gameplay == true)
+        /*if (gameplay == true)
         {
             SpawnRandomPlatform();
-        }
+        }*/
+
+        //  SpawnRandomPlatform();
+    }
+
+    private void SpawnRandomPlatform()
+    {
+        Debug.Log("SpwamRandom Call");
+        int platformIndex = Random.Range(0, platformPrefabs.Length - 1);
+        Debug.Log("PlatformIndex Set " + platformIndex);
+        Vector3 pos = new Vector3(Random.Range(-max, max), -6, 10);
+        Debug.Log("Vector pos Set " + pos);
+
+        Instantiate(platformPrefabs[platformIndex], pos, Quaternion.identity);// platformPrefabs[platformIndex].transform.rotation);
+        Debug.Log("PlatformCalling....." + platformPrefabs.ToString());
     }
 }
